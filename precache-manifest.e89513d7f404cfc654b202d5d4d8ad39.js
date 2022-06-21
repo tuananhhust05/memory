0c911e28216ef9b857b7266a001d27b5d8ed374e
Tuấn Anh Nguyễn <tuananhhust05@gmail.com>
2022-06-21 10:13:44 +0700

    Updates

---
+++ precache-manifest.e89513d7f404cfc654b202d5d4d8ad39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce92c2618630e5421807eb92b5682fa0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08d44a0d540fc6ca2a48",
+    "url": "./static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "./static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "./static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d44a0d540fc6ca2a48",
+    "url": "./static/js/main.72317f7f.chunk.js"
+  },
+  {
+    "revision": "12320bfeabed646bdf8d",
+    "url": "./static/js/runtime-main.f2e43918.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "./static/media/memories.5c0c63fb.png"
+  }
+]);
